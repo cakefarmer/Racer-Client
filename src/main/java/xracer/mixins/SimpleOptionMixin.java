fab219611e8bb27454305f826a256f525fee0109
cakefarmer <84677260+cakefarmer@users.noreply.github.com>
2022-07-17 21:06:47 -0700

    + fullbright, fixed menu bugs

--- src/main/java/xracer/mixins/SimpleOptionMixin.java
+++ src/main/java/xracer/mixins/SimpleOptionMixin.java
@@ -1,32 +1,32 @@
 package xracer.mixins;
 
-import java.util.Objects;
-import java.util.function.Consumer;
-
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.SimpleOption;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.option.SimpleOption;
 import xracer.module.settings.ISimpleOption;
+
+import java.util.Objects;
+import java.util.function.Consumer;
 
 @Mixin(SimpleOption.class)
 public class SimpleOptionMixin<T> implements ISimpleOption<T> {
 
-     T value;
-     @Final private Consumer<T> changeCallback;
-    
-    @Override
+    @Shadow
+    T value;
+    @Shadow @Final
+    private Consumer<T> changeCallback;
+
+
     public void setValueUnrestricted(T object) {
         if (!MinecraftClient.getInstance().isRunning()) {
-                this.value = object;
-                return;
-            }
-            if (!Objects.equals(this.value, object)) {
-                this.value = object;
-                this.changeCallback.accept(this.value);
-        
-            }
+            this.value = object;
+            return;
         }
+        if (!Objects.equals(this.value, object)) {
+            this.value = object;
+            this.changeCallback.accept(this.value);
+        }
+    }
 }
